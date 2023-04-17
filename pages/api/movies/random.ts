7030163652d8246e8cb83c0215cf50181a8b00df
Yanko Belov <belov.yanko@gmail.com>
2023-04-17 20:59:56 +0300

    feat: Create API endpoint for random movie

---
+++ pages/api/movies/random.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import serverAuth from "@/lib/server-auth";
+import db from "@/lib/prisma-db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", "GET");
+    return res.status(405).end("Method Not Allowed");
+  }
+
+  try {
+    await serverAuth(req);
+    const movieCount = await db.movie.count();
+
+    const randomMovie = await db.movie.findFirst({
+      skip: Math.floor(Math.random() * movieCount),
+    });
+
+    return res.status(200).json(randomMovie);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+}
