3374f39655877efe826c1a08df369f3a78056825
Yanko Belov <belov.yanko@gmail.com>
2023-04-18 12:41:59 +0300

    fix: Use the new serverAuth

--- pages/api/movies/random.ts
+++ pages/api/movies/random.ts
@@ -13,7 +13,7 @@ export default async function handler(
   }
 
   try {
-    await serverAuth(req);
+    await serverAuth(req, res);
     const movieCount = await db.movie.count();
 
     const randomMovie = await db.movie.findFirst({
